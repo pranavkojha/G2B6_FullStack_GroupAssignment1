4ab8861f70545ea10d37df2523f4dd173609dd53
Pranav Kumar Ojha <pranav.ojha@nutanix.com>
2023-12-20 05:59:13 +0530

    Test for admin class

--- G2B6_FullStack_GroupAssignment1/src/Main.java
+++ G2B6_FullStack_GroupAssignment1/src/Main.java
@@ -1,6 +1,13 @@
 
 public class Main {
+    public static void testSuperClass() {
+        SuperDepartment s = new SuperDepartment();
+        System.out.println("Department Name: " + s.departmentName());
+        System.out.println("Holida?        : " + s.isTodayAHoliday());
+        System.out.println("Today's work   : " + s.getTodaysWork());
+        System.out.println("Work deadline  : " + s.getWorkDeadline());
+    }
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        testSuperClass();
     }
 }
