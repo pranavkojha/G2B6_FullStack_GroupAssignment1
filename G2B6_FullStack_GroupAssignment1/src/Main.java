df7f15b0ee0cd9ffe2a888b54559e81e185dd4b0
Pranav Kumar Ojha <pranav.ojha@nutanix.com>
2023-12-23 12:43:54 +0530

    fixing a typo

--- G2B6_FullStack_GroupAssignment1/src/Main.java
+++ G2B6_FullStack_GroupAssignment1/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void testSuperClass() {
         SuperDepartment s = new SuperDepartment();
         System.out.println("Department Name: " + s.departmentName());
-        System.out.println("Holida?        : " + s.isTodayAHoliday());
+        System.out.println("Holiday?        : " + s.isTodayAHoliday());
         System.out.println("Today's work   : " + s.getTodaysWork());
         System.out.println("Work deadline  : " + s.getWorkDeadline());
     }
